aa8ef96a91b58d9bd418a253828e00f32632d60e
miguelzcsilva-rgb <miguelzcsilva@gmail.com>
2025-10-02 21:10:14 +0100

    exercicio de neg e pos

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+int main(){double n1;std::cout << "escreva um numero";std::cin >> n1;    if (n1 > 0) {    std::cout << "é positivo";    }else {std::cout << "é negativo";    }return 0;}
