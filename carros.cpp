00428887df6637d105222a574a529baf4e235f10
miguelzcsilva-rgb <miguelzcsilva@gmail.com>
2025-10-13 12:28:53 +0100

    Add files via upload

---
+++ carros.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	string carros[3] = {"Volvo","Mercedes","BMW"};
+	int r;
+	char sair='n';
+	do {
+		cout << "escolha um numero\n";
+		cin >> r;
+		if (r < 0 || r>2)
+			cout << "es parvo boy";
+		else{
+			cout << "queres sair do programa";
+			cin >> sair;
+		}
+	}while(sair!='s');
+	return 0;
+}
