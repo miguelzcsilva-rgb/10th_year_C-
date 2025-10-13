8a6de91c6b66c607f3666f88b2c5c55f85aadbf8
miguelzcsilva-rgb <miguelzcsilva@gmail.com>
2025-10-13 12:42:42 +0100

    Update escolhe a materia.cpp

--- escolhe a materia.cpp
+++ escolhe a materia.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
-	string carros[4] = {"portugues","historia","ciencias","Ingles"};
+	string carros[5] = {"portugues","historia","ciencias","Ingles","frances"};
 	int r;
 	char sair='n';
 	do {
 		cout << "escolha um numero \n";
 		cin >> r;
-		if (r < 0 || r>3)
+		if (r < 0 || r>4)
 			cout << "es parvo boy\n";
 		else{
 			cout << "a materia q escolheste foi " << carros[r] << ".\n";
